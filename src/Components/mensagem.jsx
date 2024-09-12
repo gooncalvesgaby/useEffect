ca6162911749f361e595f1d34a1d7f77115ae20a
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-09-12 14:54:42 -0300

    inserção useEffect

--- src/Components/mensagem.jsx
+++ src/Components/mensagem.jsx
@@ -1,12 +1,23 @@
 import * as S from "./mensagemStyle.jsx"
+import { useEffect, useState } from "react"
 
 function Mensagem() {
+    const [girls, setGirls] = useState(0)
+
+    useEffect(() => {
+        console.log('renderiza 1')
+    })
+
+    useEffect(() => {
+        console.log('renderiza')
+    }, [])
+  
     return(
         <S.Div>
             <S.Container>
                 <S.H1>GIRLS CODING</S.H1>
-                <S.P>time das girls coding {} girls</S.P>
-                <S.Button>aumente o numero do time</S.Button>
+                <S.P>time das girls coding {girls} girls</S.P>
+                <S.Button onClick={() => setGirls((girls) => girls + 1)}>aumente o número do time</S.Button>
             </S.Container>
         </S.Div>
     )
--- src/Components/mensagemStyle.jsx
+++ src/Components/mensagemStyle.jsx
@@ -3,19 +3,22 @@ import styled from "styled-components"
 export const Div = styled.div`
 background-color: #ffc0cb40;
 height: 100vh;
+`
+export const Container = styled.section`
 display: flex;
 flex-direction: column;
 align-items: center;
-`
-export const Container = styled.section`
+gap: 5rem;
 `
 
 export const H1 = styled.h1`
 font-size: 5rem;
+color: #1b1114;
 `
 
 export const P = styled.p`
 font-size: 2.5rem;
+color: ##1b1114;
 `
 
 export const Button = styled.button`
@@ -25,4 +28,5 @@ border: none;
 border-radius: 10px 15% 15px 15%;
 width: 40vw;
 height: 10vh;
+color: ##1f0810;
 `
