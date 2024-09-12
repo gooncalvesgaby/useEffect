ac064653fe8138af93916c79cebdb4dab698ca7c
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-09-12 13:09:29 -0300

    aplicando estilos

--- src/Components/mensagem.jsx
+++ src/Components/mensagem.jsx
@@ -2,9 +2,13 @@ import * as S from "./mensagemStyle.jsx"
 
 function Mensagem() {
     return(
-        <>
-        
-        </>
+        <S.Div>
+            <S.Container>
+                <S.H1>GIRLS CODING</S.H1>
+                <S.P>time das girls coding {} girls</S.P>
+                <S.Button>aumente o numero do time</S.Button>
+            </S.Container>
+        </S.Div>
     )
 }
 
--- src/Components/mensagemStyle.jsx
+++ src/Components/mensagemStyle.jsx
@@ -1,5 +1,28 @@
 import styled from "styled-components"
 
+export const Div = styled.div`
+background-color: #ffc0cb40;
+height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const Container = styled.section`
+`
+
+export const H1 = styled.h1`
+font-size: 5rem;
+`
+
 export const P = styled.p`
-color: red;
+font-size: 2.5rem;
+`
+
+export const Button = styled.button`
+font-size: 1.5rem;
+background-color: #e91e635c;
+border: none;
+border-radius: 10px 15% 15px 15%;
+width: 40vw;
+height: 10vh;
 `
